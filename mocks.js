a22d6f7af561e86c73f89bfa9a9d175677ef994e
Theophilus Omorebee <theo4u@ymail.com>
2018-05-13 12:37:32 +0100

    feat: server side implementation

---
+++ mocks.js
@@ -0,0 +1,10 @@
+module.exports = [
+  {
+    id: 'S1234X',
+    displayName: 'Theo',
+    email: 'theo4u@ymail.com',
+    type: 'bot', // bot | human
+    message: '',
+    createdAt: new Date().toISOString()
+  }
+]
---
+++ server.js
@@ -0,0 +1,35 @@
+const express = require('express')
+const bodyParser = require('body-parser')
+const Pusher = require('pusher')
+const cors = require('cors')
+require('dotenv').config()
+const shortId = require('shortid')
+let mocks = require('./mocks')
+
+const app = express()
+app.use(cors())
+app.use(bodyParser.urlencoded({ extended: false }))
+app.use(bodyParser.json())
+
+const pusher = new Pusher({
+  appId: process.env.PUSHER_APP_ID,
+  key: process.env.PUSHER_APP_KEY,
+  secret: process.env.PUSHER_APP_SECRET,
+  cluster: 'eu',
+  encrypted: true
+})
+
+app.post('/message', (req, res) => {
+  // simulate actual db save with id and createdAt added
+  const chat = {
+    id: shortId.generate(),
+    createdAt: new Date().toISOString(),
+    ...req.body
+  }
+  mocks.push(chat) // like our db
+  // trigger this update to our pushers listeners
+  pusher.trigger('chat-group', 'new', chat)
+  res.send(chat)
+})
+
+app.listen(process.env.PORT || 2000, () => console.log('Listening at 2000'))
